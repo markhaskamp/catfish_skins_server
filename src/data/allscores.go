d38330224db1a58a810e6adf47e9013b11200df9
Mark Haskamp <mark.haskamp@pinnsg.com>
2016-04-17 17:10:46 -0400

    track for 9 holes only

--- src/data/allscores.go
+++ src/data/allscores.go
@@ -24,9 +24,17 @@ type ScoreEntry struct {
 func (as *AllScores)Update(score ScoreEntry) {
   scores := as.Scores[score.Golfer]
   if len(scores) == 0 {
-    as.Scores[score.Golfer] = []int{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}    
+    as.Scores[score.Golfer] = []int{0,0,0,0,0,0,0,0,0}    
   }
-  as.Scores[score.Golfer][score.Hole] = score.Strokes
+
+  var hole int
+  if score.Hole > 9 {
+    hole = score.Hole - 10
+  } else {
+    hole = score.Hole - 1
+  }
+    
+  as.Scores[score.Golfer][hole] = score.Strokes
 }
 
 
